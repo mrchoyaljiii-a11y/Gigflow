1ce42ffcaf5b2090d4a7c9eafeb9781b995ba6bb
Satishchoyaljii <satishchoyal2@gmail.com>
2026-01-17 22:09:49 +0530

    demo2.0

--- gig_frontend/src/api/axios.js
+++ gig_frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gigflow-backend-nl4s.onrender.com",
+  baseURL: "https://gigflow-7m76.onrender.com/",
   // baseURL:"http://localhost:3000",
   withCredentials: true
 });
